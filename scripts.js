4b84d98fd555737543c316379dfd81d153aa7f3d
Liam-McLeod <liam.mcleod42@gmail.com>
2024-03-09 18:44:42 -0500

    Update

--- scripts.js
+++ scripts.js
@@ -2,18 +2,28 @@ let submitBtn = document.querySelector(".submit-btn");
 let searchBar = document.querySelector('input');
 
 submitBtn.addEventListener("click", e => {
-    var name = searchBar.value
+    var name = searchBar.value;
     getData(name);
 })
 
 async function getData(name) {
-    const res = await fetch(`https://represent.opennorth.ca/representatives/house-of-commons/?name=${name}`)
-    json = await res.json();
-    data = json.objects[0]
-    
-    
-    document.querySelector(".MP-photo").setAttribute('src',data.photo_url);
-    document.querySelector(".MP-name").innerText = data.name;
-    document.querySelector(".MP-party").innerText = data.party_name;
-    document.querySelector(".MP-constituency").innerText = data.district_name;
+
+    try {
+        const res = await fetch(`https://represent.opennorth.ca/representatives/house-of-commons/?name=${name}`);
+        json = await res.json();
+        data = json.objects[0];
+        console.log(data)
+        
+        /* Reveal MP Overview*/
+        document.querySelector(".MP-Overview").classList.remove("hidden")
+
+        document.querySelector(".MP-photo").setAttribute('src',data.photo_url);
+        document.querySelector(".MP-name").innerText = data.name;
+        document.querySelector(".MP-party").innerText = data.party_name;
+        document.querySelector(".MP-constituency").innerText = data.district_name;
+    } catch(error) {
+        console.log(error);
+        /* Keep MP Overview Hidden */
+        document.querySelector(".MP-Overview").classList.add("hidden")
+    }
 }   
